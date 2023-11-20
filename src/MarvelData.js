db2a57a00011396f0363c13abd1eada8dd785d7c
Mighty Developer <mightydeveloper500@gmial.com>
2023-11-20 15:09:30 +0100

    new project initilized

---
+++ src/MarvelData.js
@@ -0,0 +1,115 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Tony Stark",
+    alias: "Cristiano Ronaldo",
+    img1: "https://s.hs-data.com/bilder/spieler/gross/13029.jpg?fallback=png",
+    homeworld: "Earth",
+    abilities: [
+      "Cristiano Ronaldo: Portuguese football legend, prolific goal-scorer, multiple Ballon d'Or win.",
+    ],
+    team: "Cristiano Ronaldo",
+    description:
+      "Tony Stark, also known as Iron Man, is a brilliant inventor, philanthropist, and industrialist. He uses his genius-level intellect and a highly advanced powered armor suit to fight for justice and protect the world from various threats. Stark is a charismatic playboy turned superhero who combines his technological prowess with unwavering determination to uphold the values of heroism and innovation.",
+  },
+  {
+    name: "Pepper Potts",
+    alias: "Lionel Messi",
+    abilities: [
+      "Lionel Messi: Argentine football icon, prolific scorer, numerous Ballon d'Or awards.",
+    ],
+    team: "Lionel Messi",
+    img1: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt4a59805a8234ccc3/60df000cee9c2f22052cca6b/16c84589a1ceef313e45c4a39b0f28f2cbeaaf85.jpg?auto=webp&format=pjpg&width=1200&quality=60",
+    homeworld: "Earth",
+    description:
+      "Pepper Potts, the formidable Rescue, is an accomplished businesswoman and technological genius. She dons a powered armor suit equipped with an array of advanced technology and weaponry, allowing her to stand alongside the Avengers in the battle against supervillains and global threats. Pepper is a determined and fearless heroine with an unwavering commitment to justice.",
+  },
+  {
+    name: "James 'Rhodey' Rhodes",
+    alias: "Kylian Mbappé",
+    abilities: [
+      "Kylian Mbappé: French football prodigy, PSG standout, World Cup winner.",
+    ],
+    team: "Kylian Mbappé",
+    img1: "https://www.b92.net/news/pics/2023/10/29/1590130798653e664a79b37208424206_w640.jpg",
+    homeworld: "Earth",
+
+    description:
+      "Lionel Messi: Argentine football icon, prolific scorer, numerous Ballon d'Or awards.",
+  },
+  {
+    name: "Riri Williams",
+    alias: "Lewandowski",
+    abilities: [
+      "Robert Lewandowski: Prolific Polish striker, star player for Bayern Munich.",
+    ],
+    team: "Robert Lewandowski",
+    img1: "https://images.ps-aws.com/c?url=https%3A%2F%2Fimages.teamtalk.com%2Fcontent%2Fuploads%2F2022%2F03%2F25121650%2FRobert-Lewandowski-winking-while-celebrating-for-Bayern-Munich.jpg",
+    homeworld: "Earth",
+
+    description:
+      "Riri Williams, known as Ironheart, is a prodigious young inventor with a genius-level intellect. She has designed her own advanced powered armor suit, which she uses to fight against threats as a member of the Young Avengers. Riri's intelligence, determination, and innovative spirit make her a rising star in the superhero community.",
+  },
+
+  // Spider-Man
+  {
+    name: "Miles Morales",
+    alias: "Neymar Júnior",
+    abilities: [
+      "Neymar: Brazilian footballer, skilled forward, plays for Paris Saint-Germain.",
+    ],
+    team: "Neymar Júnior",
+    img1: "https://e0.365dm.com/19/05/1600x900/skysports-neymar-football-psg_4681258.jpg?20190529124515",
+    homeworld: "Earth",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
+  },
+
+  {
+    name: "Cindy Moon",
+    alias: "Karim Benzema",
+    abilities: ["Karim Benzema: French footballer, striker for Real Madrid."],
+    team: "Karim Benzema",
+    img1: "https://s.france24.com/media/display/ceebcc02-6e4f-11ee-9d46-005056a90321/w:980/p:16x9/000_33R36HD.webp",
+    homeworld: "Earth",
+    description:
+      "Cindy Moon, codenamed Silk, is a skilled hero with the power of web-shooting and remarkable superhuman agility. She is an essential member of the Agents of Atlas, using her arachnid-like abilities to combat threats and safeguard the world.",
+  },
+
+  {
+    name: "Steve Rogers",
+    alias: "Luka Modric",
+    abilities: [
+      "Luka Modrić: Croatian football midfielder, key player for Real Madrid.",
+    ],
+    team: "Luka Modric",
+    img1: "https://i.guim.co.uk/img/static/sys-images/Sport/Pix/pictures/2015/4/21/1429602536756/Luka-Modric-009.jpg?width=620&dpr=2&s=none",
+    homeworld: "Earth",
+    description:
+      "Steve Rogers, Captain America, is the embodiment of honor and virtue. Enhanced with peak human strength and wielding a Vibranium shield, he stands as a symbol of justice and resilience. Steve's unwavering commitment to the ideals of heroism makes him a cornerstone of the Avengers and the Marvel Universe.",
+  },
+  {
+    name: "Sam Wilson",
+    alias: "Kevin De Bruyne",
+    abilities: [
+      "Kevin De Bruyne: Belgian footballer, elite midfielder for Manchester City.",
+    ],
+    team: "Kevin De Bruyne",
+    img1: "https://i.guim.co.uk/img/media/400c88c3dbf2090591aa93f7c4acd1e4de5befa8/65_190_5578_3348/master/5578.jpg?width=620&dpr=2&s=none",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+];
+
+console.log(characters);
+
+// to remove initial git to push
+// rm -rf .git
+
+//2; //git init
+
+//3 git add.
+
+// 4 git commit -m "new project initialized"
