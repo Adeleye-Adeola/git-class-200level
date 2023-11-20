17a1d61057fd524bf9dc2a25e280ac3eae62a885
Mighty Developer <mightydeveloper500@gmial.com>
2023-11-20 15:20:06 +0100

    changes made

--- src/MarvelData.js
+++ src/MarvelData.js
@@ -111,5 +111,14 @@ console.log(characters);
 //2; //git init
 
 //3 git add.
-
+// copy the rest from git hub
 // 4 git commit -m "new project initialized"
+// git branch -M main
+// git remote add origin https://github.com/Adeleye-Adeola/git-class-200level.git
+// git push -u origin main
+
+// to add the changes made
+
+// 1. git add .
+// 2. git commit
+// 3 git push
